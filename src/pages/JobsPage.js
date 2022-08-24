c68c59df6fd0cbfde4a13119001ba4e7ca54d557
Sidonie Moulart <sidonie.moulart@gmail.com>
2022-08-25 00:39:32 +0200

    Add Jobs page

---
+++ src/pages/JobsPage.js
@@ -0,0 +1,23 @@
+import Header from "../components/Header/Header.js";
+import SideMenus from "../components/SideMenu/SideMenu.js";
+import Jobs from  "../components/JobsList/Jobs.js";
+
+const Links = [
+    {text:"Accueil",iconURL:"../../public/assets/svg-icons/home.svg",url:"./",title:""},
+    {text:"Les jobs",iconURL:"../../public/assets/svg-icons/job.svg",url:"./",title:""},
+    {text:"Messages",iconURL:"../../public/assets/svg-icons/message.svg",url:"./",title:""},
+]
+
+const logoUrl = "../../public/assets/logo.png"
+const logoName = "Jobtalk"
+
+async function JobsPage() {
+    document.title = "Les Jobs-JobTalk";
+    const MAIN = document.querySelector('main');
+    const HEADER = document.querySelector('header');
+    document.body.appendChild(SideMenus({logoUrl,logoName, linksArray:Links}));
+    MAIN.appendChild(await Jobs());
+    HEADER.appendChild(Header({userName:"Eva Barnett", title:"Les Jobs"}));
+}
+
+export default JobsPage;
